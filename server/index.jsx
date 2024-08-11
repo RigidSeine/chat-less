ffb64825be4dc420a5a12938672d2b0fb995a724
Ricky Leong <rickyhhleong@gmail.com>
2024-08-12 00:06:22 +1200

    Added behaviour for when a user joins a room

--- server/index.jsx
+++ server/index.jsx
@@ -17,15 +17,37 @@ const io = new Server(server, {
     }
 });
 
+const chatbot = 'ChatBot';
+let chatRoom = '';
+let allUsers = [];
+
 //Listen for when a client connects via socket.io-client.
 io.on('connection', (socket) => {
     console.log(`User connected with ID: ${socket.id}`);
 
     socket.on('join_room', (data) => {
         const {username, room}  = data; //Expecting username and room to be returned by the client
-        socket.join(room); 
+        socket.join(room); //Join the user to a socket room
+    
+        let createdTime = Date.now();
+        //Send message to all users joined to the room
+        //Emit the 'receive_message' event and pass through an object with some details about the event
+        socket.to(room).emit('receive_message', {
+            message: `${username} has joined the chat room`,
+            username: chatbot,
+            createdTime //Note the shorthand notation - this is the equivalent of createdTime: createdTime
+        });
+
+        chatRoom = room;
+        allUsers.push({id: socket.id, username, room});
+        
+        var chatRoomUsers = allUsers.filter((user) => user.room == room);
+        socket.to(room).emit('chatroom_users', chatRoomUsers);
+        socket.emit('chatroom_users', chatRoomUsers); //Why global emit
     }
-    )
+    );
+
+
 });
 
 //Set up the port that server is running on
