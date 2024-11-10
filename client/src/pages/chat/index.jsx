45c4193d2a2cd6513796808c5e368ad8ba4882f7
Ricky Leong <rickyhhleong@gmail.com>
2024-11-10 23:47:57 +1300

    Added users list in room

--- client/src/pages/chat/index.jsx
+++ client/src/pages/chat/index.jsx
@@ -1,10 +1,12 @@
 import styles from './styles.module.css';
 import Messages from './messages';
 import SendMessage from './send-message';
+import RoomAndUsers from './room-and-users';
 
 const Chat = ({socket, username, room}) => {
     return (
         <div className={styles.chatContainer}>
+            <RoomAndUsers socket={socket} room={room} username={username}/>
             <div>
                 <Messages socket={socket}/>
                 <SendMessage socket={socket} username={username} room={room}/>
